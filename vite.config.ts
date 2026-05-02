1848d93e94ccb70e11d9d056a58b63851823a577
Jeewoo Choi <jchoi23@stanford.edu>
2026-05-02 15:45:14 -0700

    Got the basic cesium map working, I want it to be a bit more detailed, I'll go ahead and do that soon.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,40 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+
+const cesiumSource = 'node_modules/cesium/Build/Cesium'
+const cesiumBaseUrl = 'cesiumStatic'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  define: {
+    CESIUM_BASE_URL: JSON.stringify(cesiumBaseUrl),
+  },
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: `${cesiumSource}/ThirdParty`,
+          dest: cesiumBaseUrl,
+          rename: { stripBase: 4 },
+        },
+        {
+          src: `${cesiumSource}/Workers`,
+          dest: cesiumBaseUrl,
+          rename: { stripBase: 4 },
+        },
+        {
+          src: `${cesiumSource}/Assets`,
+          dest: cesiumBaseUrl,
+          rename: { stripBase: 4 },
+        },
+        {
+          src: `${cesiumSource}/Widgets`,
+          dest: cesiumBaseUrl,
+          rename: { stripBase: 4 },
+        },
+      ],
+    }),
+  ],
 })
